e14f7acc5f0107d2e8b992f3d7b0d407836ce0d8
Rosiel Victor <codepremium917@gmail.com>
2024-05-26 00:41:25 -0300

    feat: Salvar a URL da API no localStorage

--- src/components/home/getApiUrlForm.tsx
+++ src/components/home/getApiUrlForm.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {useEffect, useState} from "react"
+import React, {use, useEffect, useState} from "react"
 import { useFormStatus } from "react-dom";
 import {z} from "zod"
 import { Button } from "@/components/ui/button"
@@ -16,24 +16,27 @@ type Props = {
 }
 export function GetApiUrlForm({ia_api_placeholder, ia_api_button, ia_api_error, ia_api_loading, ia_api_connected, defaultUrl}: Props) {
     const [url, setUrl] = useState(defaultUrl)
-    const { pending, data, method, action } = useFormStatus();
+    const { pending, data } = useFormStatus();
     const { toast } = useToast();
     const schema = z.string().url()
 
     useEffect(() => {
         if (data) {
-            fetch(schema.parse(data.get("url")), {
+            const url = data.get("url")
+            fetch(schema.parse(url), {
                 method: "GET",
                 headers: {
                     'Content-Type': 'application/json'
                 }
             }).then(response => {
-                console.log(response)
                 if (response.ok) {
                     toast({
                         title: 'API Connected',
                         description: ia_api_connected,
                     })
+
+                    // save the url in local storage
+                    localStorage.setItem('ia_api_url', schema.parse(url))
                 } else {
                     toast({
                         title: 'API Error',
