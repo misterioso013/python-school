610ac5377fdeeefeddee50b5065fa0feb8080d39
Rosiel Victor <codepremium917@gmail.com>
2024-05-26 00:35:57 -0300

    feat: Adicionar pacote @radix-ui/react-toast para exibir notificações

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,6 +3,7 @@ import "../globals.css";
 import { cn } from "@/lib/utils";
 import { getTranslations } from "next-intl/server";
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -41,6 +42,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <Toaster />
             </ThemeProvider>
           </body>
     </html>
